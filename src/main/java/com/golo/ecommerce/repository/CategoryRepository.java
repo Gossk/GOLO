851a38a70e11af3dd5debc87efe44aabda096824
Gossk <vidalmiguel535@gmail.com>
2026-03-06 23:26:02 -0500

    feat: add JPA repositories with custom queries

---
+++ src/main/java/com/golo/ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,20 @@
+package com.golo.ecommerce.repository;
+
+import com.golo.ecommerce.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findByActiveTrueAndParentIsNull();
+
+    List<Category> findByParentIdAndActiveTrue(Long parentId);
+
+    Optional<Category> findByName(String name);
+
+    Boolean existsByName(String name);
+}
